af164e6e90bc64ae0429cb703323ab5c5b3af274
bv-ajay27 <ajayab7799@gmail.com>
2022-06-11 22:58:44 +0530

    calculation pending

--- FrontEnd_App/tip_Calculator/script.js
+++ FrontEnd_App/tip_Calculator/script.js
@@ -2,23 +2,37 @@ function getRating(){
     const taste = document.getElementById("tasteRating").value;
     const color = document.getElementById("colorRating").value;
     const aroma = document.getElementById("aromaRating").value;
-    const sum = ((taste + color + aroma)/3);
-    document.getElementById("foodRating").innerHTML = "Rating : " + sum;
+    const avg = parseInt((taste+color+aroma)/3);
+    document.getElementById("foodRating").innerHTML = "Rating : " + avg;
 
     // servicing
     const service = document.getElementById("waiter").value;
-    const servicing = service;
-    document.getElementById("ServiceRating").innerHTML = "Service Rating : " + servicing;
+    document.getElementById("ServiceRating").innerHTML = "Service Rating : " + service;
 
     // Hygene
     const staff = document.getElementById("staffHygene").value;
     const over = document.getElementById("overallLook").value;
-    const overall = ((staff + over)/2);
+    const overall = parseInt((staff+over)/2);
     document.getElementById("HygeneRating").innerHTML = "Hygene Rating : " + overall;
+    
+    // final rating
+    const finalRating = (avg+service+overall)/3;
     
     // bill amount
     const bill = document.getElementById("billAmount").value;
+    // document.getElementById("tipAmount").innerHTML = "Tip Amount : " (bill*4)*100;
 
     //final tip
-    document.getElementById("tipAmount").innerHTML = "Tip Amount : " (bill*overall);
+    if(finalRating > 3)
+    {
+        document.getElementById("tipAmount").innerHTML = "Tip Amount : " ((bill*4)*100);
+    }
+    else if(finalRating >= 2 && finalRating < 4)
+    {
+        document.getElementById("tipAmount").innerHTML = "Tip Amount : " ((bill*3)*100);
+    }
+    else
+    {
+        document.getElementById("tipAmount").innerHTML = "Tip Amount : " ((bill*2)*100);
+    }
 }
