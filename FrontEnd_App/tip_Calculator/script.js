0b98c4fa10f21bde9c36ca525b001624fce4d0fa
bv-ajay27 <ajayab7799@gmail.com>
2022-06-12 19:32:04 +0530

    tip

--- FrontEnd_App/tip_Calculator/script.js
+++ FrontEnd_App/tip_Calculator/script.js
@@ -1,38 +1,41 @@
 function getRating(){
-    const taste = document.getElementById("tasteRating").value;
-    const color = document.getElementById("colorRating").value;
-    const aroma = document.getElementById("aromaRating").value;
-    const avg = parseInt((taste+color+aroma)/3);
+    const taste = parseInt(document.getElementById("tasteRating").value);
+    const color = parseInt(document.getElementById("colorRating").value);
+    const aroma = parseInt(document.getElementById("aromaRating").value);
+    const avg = (taste+color+aroma)/3;
     document.getElementById("foodRating").innerHTML = "Rating : " + avg;
 
     // servicing
-    const service = document.getElementById("waiter").value;
+    const service = parseInt(document.getElementById("waiter").value);
     document.getElementById("ServiceRating").innerHTML = "Service Rating : " + service;
 
     // Hygene
-    const staff = document.getElementById("staffHygene").value;
-    const over = document.getElementById("overallLook").value;
-    const overall = parseInt((staff+over)/2);
+    const staff = parseInt(document.getElementById("staffHygene").value);
+    const over = parseInt(document.getElementById("overallLook").value);
+    const overall = (staff+over)/2;
     document.getElementById("HygeneRating").innerHTML = "Hygene Rating : " + overall;
     
     // final rating
-    const finalRating = (avg+service+overall)/3;
+    const finalRating = parseInt((avg+service+overall)/3);
+
+    // print rating
+    document.getElementById("rating").innerHTML = "Rating : " + finalRating;
     
     // bill amount
-    const bill = document.getElementById("billAmount").value;
+    const bill = parseInt(document.getElementById("billAmount").value);
     // document.getElementById("tipAmount").innerHTML = "Tip Amount : " (bill*4)*100;
 
     //final tip
     if(finalRating > 3)
     {
-        document.getElementById("tipAmount").innerHTML = "Tip Amount : " ((bill*4)*100);
+        document.getElementById("tipAmount").innerHTML = "Tip Amount : " + (bill*(3/100));
     }
     else if(finalRating >= 2 && finalRating < 4)
     {
-        document.getElementById("tipAmount").innerHTML = "Tip Amount : " ((bill*3)*100);
+        document.getElementById("tipAmount").innerHTML = "Tip Amount : " + (bill*(2/100));
     }
     else
     {
-        document.getElementById("tipAmount").innerHTML = "Tip Amount : " ((bill*2)*100);
+        document.getElementById("tipAmount").innerHTML = "Tip Amount : " + (bill*(1/100));
     }
 }
