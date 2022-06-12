2324a59d2707d6a9840aa2f3ab87d449c3e1bcae
bv-ajay27 <ajayab7799@gmail.com>
2022-06-12 17:32:08 +0530

    clock

--- FrontEnd_App/Clock/script.js
+++ FrontEnd_App/Clock/script.js
@@ -9,14 +9,14 @@ function time(){
 
     const sec = time.getSeconds();
     const secDeg = ((sec / 60) * 360) + 90;
-    secHand.style.transform = `rotate(${secDeg}deg)`; 
+    secHand.style.transform = `rotate(${secDeg}deg)`;
 
-    const min = time.getminonds();
-    const minDeg = ((min / 60) * 360) + 90;
+    const min = time.getMinutes();
+    const minDeg = ((min / 60) * 360) + ((sec/60)*6) + 90;
     minHand.style.transform = `rotate(${minDeg}deg)`;
 
-    const hour = time.getSeconds();
-    const hourDeg = ((hour / 60) * 360) + 90;
+    const hour = time.getHours();
+    const hourDeg = ((hour / 12) * 360) + ((min/60)*30) + 90;
     hourHand.style.transform = `rotate(${hourDeg}deg)`;
 }
 
