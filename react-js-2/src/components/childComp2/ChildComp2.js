2a218fefb4f261d3712773a6a9fb43517daf76f7
bv-ajay27 <ajayab7799@gmail.com>
2022-06-14 23:38:47 +0530

    react

--- react-js-2/src/components/childComp2/ChildComp2.js
+++ react-js-2/src/components/childComp2/ChildComp2.js
@@ -6,8 +6,8 @@ const ChildComp2 = () =>{
     return(
         <div className="ChildComp2">
             <h1>This is Child2</h1>
-            <SubChildComp2/>
-            <SubChildComp2/>
+            {/* <SubChildComp2 title="classBasedProps"/> */}
+            <SubChildComp2 count = "0"/>
             <SubChild3 title="Heading-1" color="yellow" backgroundColor="pink"/>
             <SubChild3 color="voilet" backgroundColor="white"/>
         </div>
--- react-js-2/src/components/childComp2/SubChildComp2.js
+++ react-js-2/src/components/childComp2/SubChildComp2.js
@@ -1,9 +1,45 @@
 import React, {Component} from "react";
 
 class SubChildComp2 extends Component{
+    constructor(props){
+        super(props)
+
+        this.state = {
+            name: "ram"
+        }
+        this.state ={
+            count :0
+        }
+    }
+    changeName = () =>{
+        this.setState({
+            name:"Ram Kumar"
+        })
+    }
+    increnment = ()=>{
+        this.setState({
+            count : this.state.count + 1
+        })
+    }
+    decrement = () =>{
+        this.setState({
+            count:this.state.count - 1
+        })
+    }
     render(){
+        // console.log(this.name)
+        let {title} = this.state
+        let {count} = this.state
         return(
-            <h1>This is SubChild of Comp-2</h1>
+            <>
+                <h1>This is SubChild of Comp-2</h1>
+                <h2>It using class base componenets</h2>
+                <p>{title} -- This is the title from the SubChildComp2 using props</p>
+                <button onClick={this.changeName}>Change Name</button>
+                <p style={{fontSize :"5em"}} onClick={this.increnment}>{count}</p>
+                <button style={{fontSize:"20px"}} onClick={this.decrement}>Decreament</button>
+                <button style={{fontSize:"20px"}} onClick={this.increnment}>increnment</button>
+            </>
         )
     }
 }
