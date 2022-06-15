ee90d7e9d63ef11a8072bb338dd8ff06cf73d5a8
bv-ajay27 <ajayab7799@gmail.com>
2022-06-15 11:01:06 +0530

    clock

--- FrontEnd_App/Clock/script.js
+++ FrontEnd_App/Clock/script.js
@@ -8,16 +8,18 @@ function time(){
     const time = new Date();
 
     const sec = time.getSeconds();
-    const secDeg = ((sec / 60) * 360) + 90;
+    const secDeg = ((sec / 60) * 360)+270;
     secHand.style.transform = `rotate(${secDeg}deg)`;
 
     const min = time.getMinutes();
-    const minDeg = ((min / 60) * 360) + ((sec/60)*6) + 90;
+    const minDeg = ((min / 60) * 360) + ((sec/60)*6)+270;
     minHand.style.transform = `rotate(${minDeg}deg)`;
 
     const hour = time.getHours();
-    const hourDeg = ((hour / 12) * 360) + ((min/60)*30) + 90;
+    const hourDeg = ((hour / 12) * 360) + ((min/60)*30)+270;
     hourHand.style.transform = `rotate(${hourDeg}deg)`;
+
+    console.log(sec,min,hour);
 }
 setInterval(time,1000);
 
--- FrontEnd_App/tip_Calculator/script.js
+++ FrontEnd_App/tip_Calculator/script.js
@@ -3,23 +3,23 @@ function getRating(){
     const color = parseInt(document.getElementById("colorRating").value);
     const aroma = parseInt(document.getElementById("aromaRating").value);
     const avg = (taste+color+aroma)/3;
-    document.getElementById("foodRating").innerHTML = "Food Rating : " + avg;
+    document.getElementById("foodRating").innerHTML = "Food Rating : " + avg.toFixed(1);
 
     // servicing
     const service = parseInt(document.getElementById("waiter").value);
-    document.getElementById("ServiceRating").innerHTML = "Service Rating : " + service;
+    document.getElementById("ServiceRating").innerHTML = "Service Rating : " + service.toFixed(1);
 
     // Hygene
     const staff = parseInt(document.getElementById("staffHygene").value);
     const over = parseInt(document.getElementById("overallLook").value);
     const overall = (staff+over)/2;
-    document.getElementById("HygeneRating").innerHTML = "Hygene Rating : " + overall;
+    document.getElementById("HygeneRating").innerHTML = "Hygene Rating : " + overall.toFixed(1);
     
     // final rating
     const finalRating = parseInt((avg+service+overall)/3);
 
     // print rating
-    document.getElementById("rating").innerHTML = "Overall Rating : " + finalRating;
+    document.getElementById("rating").innerHTML = "Overall Rating : " + finalRating.toFixed(1);
     
     // bill amount
     const bill = parseInt(document.getElementById("billAmount").value);
@@ -28,14 +28,14 @@ function getRating(){
     //final tip
     if(finalRating > 3)
     {
-        document.getElementById("tipAmount").innerHTML = "Tip Amount : " + (bill*(1/100));
+        document.getElementById("tipAmount").innerHTML = "Tip Amount : " + (bill*(4/100));
     }
     else if(finalRating >= 2 && finalRating < 4)
     {
-        document.getElementById("tipAmount").innerHTML = "Tip Amount : " + (bill*(0.5/100));
+        document.getElementById("tipAmount").innerHTML = "Tip Amount : " + (bill*(2/100));
     }
     else
     {
-        document.getElementById("tipAmount").innerHTML = "Tip Amount : " + (bill*(0.25/100));
+        document.getElementById("tipAmount").innerHTML = "Tip Amount : " + (bill*(1/100));
     }
 }
