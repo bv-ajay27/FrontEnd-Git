1c55037a8723d7fa26539971f960eb4d96ebc393
bv-ajay27 <ajayab7799@gmail.com>
2022-06-22 18:18:43 +0530

    changes in construction app

--- MyFrontEnd-Apps/construction-App/script.js
+++ MyFrontEnd-Apps/construction-App/script.js
@@ -1,41 +1,30 @@
-let searchForm=document.querySelector('.search-form');
-document.querySelector('#search-btn').onclick= () =>{
-    searchForm.classList.toggle('active');
+let loginForm=document.querySelector('.login-form');
+document.querySelector('#user-btn').onclick= () =>{
+    loginForm.classList.toggle('active');
+    shoppingCart.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let navbar=document.querySelector('.options');
+document.querySelector('#menu-btn').onclick= () =>{
+    navbar.classList.toggle('active');
     shoppingCart.classList.remove('active');
     loginForm.classList.remove('active');
-    navbar.classList.remove('active');
 }
 
 let shoppingCart=document.querySelector('.shopping-cart');
 document.querySelector('#cart-btn').onclick= () =>{
     shoppingCart.classList.toggle('active');
-    searchForm.classList.remove('active');
     loginForm.classList.remove('active');
     navbar.classList.remove('active');
-}
-
-let loginForm=document.querySelector('.login-form');
-document.querySelector('#login-btn').onclick= () =>{
-    loginForm.classList.toggle('active');
-    searchForm.classList.remove('active');
-    shoppingCart.classList.remove('active');
-    navbar.classList.remove('active');
-}
-
-let navbar=document.querySelector('.navbar');
-document.querySelector('#menu-btn').onclick= () =>{
-    navbar.classList.toggle('active');
-    searchForm.classList.remove('active');
-    shoppingCart.classList.remove('active');
-    loginForm.classList.remove('active');
 }
 
 window.onscroll = () =>{
-    searchForm.classList.remove('active');
-    shoppingCart.classList.remove('active');
     loginForm.classList.remove('active');
+    // shoppingCart.classList.remove('active');
     navbar.classList.remove('active');
 }
+
 
 var swiper = new Swiper(".product-slider", {
     loop:true,
@@ -78,3 +67,90 @@ var swiper = new Swiper(".review-slider", {
       },
     },
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let searchForm=document.querySelector('.search-form');
+// document.querySelector('#search-btn').onclick= () =>{
+//     searchForm.classList.toggle('active');
+//     shoppingCart.classList.remove('active');
+//     loginForm.classList.remove('active');
+//     navbar.classList.remove('active');
+// }
+
+// let shoppingCart=document.querySelector('.shopping-cart');
+// document.querySelector('#cart-btn').onclick= () =>{
+//     shoppingCart.classList.toggle('active');
+//     searchForm.classList.remove('active');
+//     loginForm.classList.remove('active');
+//     navbar.classList.remove('active');
+// }
+
+// let loginForm=document.querySelector('.login-form');
+// document.querySelector('#login-btn').onclick= () =>{
+//     loginForm.classList.toggle('active');
+//     searchForm.classList.remove('active');
+//     shoppingCart.classList.remove('active');
+//     navbar.classList.remove('active');
+// }
+
+// let navbar=document.querySelector('.navbar');
+// document.querySelector('#menu-btn').onclick= () =>{
+//     navbar.classList.toggle('active');
+//     searchForm.classList.remove('active');
+//     shoppingCart.classList.remove('active');
+//     loginForm.classList.remove('active');
+// }
+
+// window.onscroll = () =>{
+//     searchForm.classList.remove('active');
+//     shoppingCart.classList.remove('active');
+//     loginForm.classList.remove('active');
+//     navbar.classList.remove('active');
+// }
+
+// // function showAbout()
+// // {
+// //     document.getElementById('aboutStore').innerHTML=" non similique repudiandae amet rerum quasi odio expedita cum non similique repudiandae amet rerum quasi odio expedita cum non similique repudiandae amet rerum quasi odio expedita cum non similique repudiandae amet rerum quasi odio expedita cum";
+// // }
