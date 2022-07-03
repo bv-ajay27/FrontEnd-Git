d7901ed345cb4a448f85e57eaf35c0d043bd327d
bv-ajay27 <ajayab7799@gmail.com>
2022-07-03 23:00:35 +0530

    photo app

--- React/photos-app/src/App.js
+++ React/photos-app/src/App.js
@@ -1,11 +1,19 @@
+import {Route,Routes,BrowserRouter} from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
+import Search from './components/Search';
+
 
 
 function App() {
   return (
     <>
-      <Home/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='Search' element={<Search/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- React/photos-app/src/components/Home.js
+++ React/photos-app/src/components/Home.js
@@ -1,32 +1,46 @@
-import React from 'react'
+import React,{useState} from 'react'
+import {useHistory} from 'react-router-dom'
 import "./style.css"
+import Search from './Search'
 
 
 const Home = () => {
+    const [imgName,setImgName] = useState('');
+    let history = useHistory();
   return (
     <>
-        <div className='container'>
-            <div className='navBar'>
-                <h3 className='brand'>Pixabay</h3>
-                <div className='auth-btn'>
-                    <button>SignUp</button>
-                    <button>Login</button>
-                </div>
-            </div>
-            <div className='search'>
-                <h2>Stunning free images & royalty free stock</h2>
-                <h4>
-                    Over 2.6 million+ high quality stock images, videos and music shared by our talented community.
-                </h4>
-                <br/>
-                <div className='searchBar'>
-                    <input type="text"placeholder='serach...'/>
-                </div><br/>
-                <button onClick>Search</button>
-            </div>
+      <div className="container">
+        <div className="navBar">
+          <ul>
+            <li>
+              <h3 className="brand">
+                Pixabay<sub>get Photos..</sub>
+              </h3>
+            </li>
+            <li style={{ float: "right" }}>
+              <button>SignUp</button>
+            </li>
+            <li style={{ float: "right" }}>
+              <button>Login</button>
+            </li>
+          </ul>
         </div>
+        <div className="search">
+          <h2>Stunning free images & royalty free stock</h2>
+          <h4>
+            Over 2.6 million+ high quality stock images, videos and music shared
+            by our talented community.
+          </h4>
+          <br />
+          <div className="searchBar">
+            <input type="text" placeholder="serach..." onChange={(e) => setImgName(e.target.value)}/>
+          </div>
+          <br />
+          <button onClick={history.push('/Search')}>Search</button>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
 export default Home
