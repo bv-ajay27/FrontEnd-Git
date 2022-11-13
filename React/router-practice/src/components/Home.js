4667aa07ce375c8b88bce5384935ab5fe2694e44
bv-ajay27 <ajayab7799@gmail.com>
2022-11-13 18:41:37 +0530

    aws

--- React/router-practice/src/components/Home.js
+++ React/router-practice/src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './style.css'
+import logo from "./images/logo.jpg"
 import { Link } from "react-router-dom";
 
 
@@ -9,19 +10,25 @@ const Home = () => {
     <div className='main'>
       <div className='header'>
         <div className='Logo'>
-          <img src="images/logo.jpg" alt='logo'></img>
+          <img src= {logo} alt="logo" style={{height : "30px",width:"40px"}}/>
+          {/* // <img src="images/logo.jpg" alt='logo'></img> */}
           <span>LeanPitch</span>
         </div>
         <button className='contactUs'>Contact Us</button>
         <button className='subscribe'>Subscribe</button>
       </div>
-      <div>
+      <div className='links'>
           <nav>
-              {/* <Link to="/"><li>Home</li></Link> */}
-              <Link to="/About"><li>About</li></Link>
-              <Link to="/ContactUs"><li>ContactUs</li></Link>
+              <Link to="/Trainings"><li>Trainings</li></Link>
               <Link to="/OurMeetUps"><li>Our MeetUps</li></Link>
-              <Link to="/Register"><li>Register</li></Link>
+              <Link to="/Webinars"><li>Webinars</li></Link>
+              <Link to="/Conference"><li>Conference</li></Link>
+              <Link to="/Videos"><li>Videos</li></Link>
+              <Link to="/PM-Speak"><li>PM Speak Series</li></Link>
+              <Link to="/Be-speaker"><li>Be a Speaker</li></Link>
+              <Link to="/Podcasts"><li>Podcasts</li></Link>
+              <Link to="/Blogs"><li>Blogs</li></Link>
+              <Link to="/About"><li>About Us</li></Link>
           </nav>
       </div>
     </div>
